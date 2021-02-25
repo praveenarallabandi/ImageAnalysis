c8b910bb8007800369e61d5924cff8e999c77240
praveenakondepudi <praveenakondepudi@gmail.com>
2021-02-24 23:48:05 -0500

    In progress

--- ImageAnalysisPart1.py
+++ ImageAnalysisPart1.py
@@ -30,12 +30,41 @@ def process_image(entry):
     print("----------------------------------------------------")
     print("The shape of the original image array is: ", img.shape)
     # Save the output image
-    print("... Save the output image")
+    """ print("... Save the output image")
     img.astype('int8').tofile('NewImage.raw')
-    print("... File successfully saved")
-    # Closing the file
+    print("... File successfully saved") """
+    result = noisy('gaussian', img)
+    print('Gaussian Result - ')
+    print(result)
     entry.close()
 
+def noisy(noise_typ, image):
+   if noise_typ == "gaussian":
+      row,col= image.shape
+      mean = 0
+      var = 0.1
+      sigma = var**0.5
+      gauss = np.random.normal(mean,sigma,(row,col))
+      gauss = gauss.reshape(row,col)
+      noisy = image + gauss
+      return noisy
+   elif noise_typ == "s&p":
+      row,col,ch = image.shape
+      s_vs_p = 0.5
+      amount = 0.004
+      out = np.copy(image)
+      # Salt mode
+      num_salt = np.ceil(amount * image.size * s_vs_p)
+      coords = [np.random.randint(0, i - 1, int(num_salt))
+              for i in image.shape]
+      out[coords] = 1
+
+      # Pepper mode
+      num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
+      coords = [np.random.randint(0, i - 1, int(num_pepper))
+              for i in image.shape]
+      out[coords] = 0
+      return out
 basepath = process_batch()
 
 
